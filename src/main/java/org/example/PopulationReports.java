98be5aa6507c05345250936c31ef6d03e62e1a55
ykaraha5 <40692347@live.napier.ac.uk>
2024-10-29 16:58:32 +0000

    initial commit

---
+++ src/main/java/org/example/PopulationReports.java
@@ -0,0 +1,18 @@
+package org.example;
+
+class PopulationReports {
+
+    public void main(String[] args) {
+        PopulationReports Reports = new PopulationReports();
+
+        System.out.println("Countries by population:");
+        Reports.getClass();
+
+        System.out.println("\nCountries in the world by population:");
+        Reports.getClass();
+
+        System.out.println("\nTop 5 populated countries in the world:");
+        Reports.getClass();
+    }
+}
+
