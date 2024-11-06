be3c478b58ef7088b127c3b77656d729fcc27636
ykaraha5 <40692347@live.napier.ac.uk>
2024-11-06 12:08:38 +0000

    initial commit

--- src/main/java/org/example/PopulationReports.java
+++ src/main/java/org/example/PopulationReports.java
@@ -1,18 +1,29 @@
 package org.example;
 
-class PopulationReports {
+public class PopulationReports {
 
-    public void main(String[] args) {
-        PopulationReports Reports = new PopulationReports();
+    public static void main(String[] args) {
+        // Create an instance of PopulationReports
+        PopulationReports reports = new PopulationReports();
 
+        // Display countries by population
         System.out.println("Countries by population:");
-        Reports.getClass();
+        reports.getCountriesByPopulation();
 
-        System.out.println("\nCountries in the world by population:");
-        Reports.getClass();
-
+        // Display top 5 populated countries in the world
         System.out.println("\nTop 5 populated countries in the world:");
-        Reports.getClass();
+        reports.getTopNCountriesByPopulation(5);
+    }
+
+    // Example methods to be implemented
+    public void getCountriesByPopulation() {
+        // Implementation goes here (e.g., fetching and displaying data from the database)
+        System.out.println("Fetching all countries by population...");
+    }
+
+    public void getTopNCountriesByPopulation(int n) {
+        // Implementation goes here (e.g., fetching and displaying data from the database)
+        System.out.println("Fetching top " + n + " populated countries...");
     }
 }
 
